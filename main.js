199e84c4a55fcb7c817b7d01d3ed7738017a8a4d
Julia <julia.chernetskaja@gmail.com>
2017-12-05 15:25:36 +0200

    fixed logic

--- main.js
+++ main.js
@@ -6,23 +6,41 @@ let CardModel = function (data) {
 	}, 
 
 	Results = function (data) {
-		// this.tab = tab,
 		this.user = data.user,
 		this.score = data.score,
-		this.date = data.date
-
+		this.date = (new Date(data.date)).toLocaleString('en-GB'),
 		this.cell = function () {
 			return `
-			// <table>
-			// 	<tbody>
-					<tr>
-						<td>${this.user}</td>
-						<td>${this.score}</td>
-						<td>${this.date}</td>
-					</tr>
-			// 	</tbody>
-			// </table>`
+				<tr>
+					<td>${this.user}</td>
+					<td>${this.score}</td>
+					<td>${this.date}</td>
+				</tr>
+			`
 		}
+		
+	},
+
+	ResultTabCreate = function (rows) {
+		this.rows = rows,
+		this.tabCreate = function (rows) {
+			return `
+				<table>
+					<tbody>
+						${this.rows}
+					</tbody>
+				</table>
+			`
+		}
+	},
+
+	pageResults = {
+		// tabName: [],
+		pages: {}
+		
+	// }
+
+
 	},
 
 	temporaryData = {
@@ -38,7 +56,7 @@ let CardModel = function (data) {
 		time: 0,
 		score: 100000,
 		getUsername: function () {
-	    	this.userName = document.getElementById("user").value + '';	
+			this.userName = document.getElementById("user").value + '';	
 		}
 	},
 
@@ -49,11 +67,42 @@ let CardModel = function (data) {
 		chooseUser: document.getElementById('submit'),
 		area: document.getElementById('selectAreaSize'),
 		showScore: document.getElementById('scores'),
+		// resultsContainer: '',
+		// tabsContainer: '',
 		getStorage: function () {
 			return JSON.parse(localStorage.getItem('pair'))
 		},
 		setStorage: function (obj) {
 			localStorage.setItem('pair', JSON.stringify(obj))
+		}
+	},
+
+	// button = 
+
+	popUp = {
+		
+		content: function (data, fun) {
+			let popUpContainer = document.createElement('div'),
+			popUpContant = document.createElement('div'),
+			button = document.createElement('div');
+			popUpContainer.id = 'cover';
+			popUpContant.className = 'popup';
+			popUpContainer.appendChild(popUpContant);
+			button.innerHTML = 'Ok';
+			console.log(data)
+			popUpContant.appendChild(data);
+			popUpContant.appendChild(button);
+			
+			setTimeout(function () {
+				document.body.appendChild(popUpContainer);
+			}, 700);
+
+			button.onclick = function () {
+				document.getElementById('cover').remove();
+				fun();
+			}
+
+			// timer.start();
 		}
 	},
 
@@ -65,19 +114,19 @@ let CardModel = function (data) {
 			menu.continueBtn.style.display = 'none';
 			
 			this.interval = setInterval(function () {
-			  	if(+time.s.innerHTML < 59 && +time.m.innerHTML !== 59) {
-			  		time.s.innerHTML = time.s.innerHTML++ < 9 
-			  			? ('0' + time.s.innerHTML++) : time.s.innerHTML++;
-			  	}else if (+time.s.innerHTML < 59 && +time.m.innerHTML < 59){
-			  		time.s.innerHTML = '00';
-			  		time.m.innerHTML = '00';
-			  		time.h.innerHTML = time.h.innerHTML++ < 9
-			  			? ('0' + time.h.innerHTML++) : time.h.innerHTML++;
-			  	}else{
-			  		time.m.innerHTML = time.m.innerHTML++ < 9
-			  			? ('0' + time.m.innerHTML++) : time.m.innerHTML++;
-			  		time.s.innerHTML = '00';
-			  	}
+				if(+time.s.innerHTML < 59 && +time.m.innerHTML !== 59) {
+					time.s.innerHTML = time.s.innerHTML++ < 9 
+						? ('0' + time.s.innerHTML++) : time.s.innerHTML++;
+				}else if (+time.s.innerHTML < 59 && +time.m.innerHTML < 59){
+					time.s.innerHTML = '00';
+					time.m.innerHTML = '00';
+					time.h.innerHTML = time.h.innerHTML++ < 9
+						? ('0' + time.h.innerHTML++) : time.h.innerHTML++;
+				}else{
+					time.m.innerHTML = time.m.innerHTML++ < 9
+						? ('0' + time.m.innerHTML++) : time.m.innerHTML++;
+					time.s.innerHTML = '00';
+				}
 			}, 1000)
 		},
 
@@ -116,23 +165,30 @@ let CardModel = function (data) {
 //Controller
 let controller = {
 	init: function() {
-		menu.chooseUser.onclick=function () {
-			game.getUsername();
-			timer.stop();
-	    	timer.start();
-		}
+		
 		menu.continueBtn.onclick = this.continueGame;
 		menu.pauseBtn.onclick = this.pauseGame;
 		menu.showScore.onclick = this.userScores;
 		
-        cardView.init();
-    },
+		cardView.init();
+		console.log('Ok')
+	},
 
-    changeArea: function () {
+	changeUser: function () {
+		menu.chooseUser.onclick=function () {
+			// game.getUsername();
+			cardView.init();
+			console.log(game.userName)
+			timer.stop();
+			timer.start();
+		}
+	},
+
+	changeArea: function () {
 		menu.area.onchange=function () {
-	    		cardView.init();
-	    		timer.stop();
-	    		timer.start();	
+			cardView.init();
+			timer.stop();
+			timer.start();	
 		};
 	},
 
@@ -144,72 +200,87 @@ let controller = {
 		timer.start();
 	},
 
-    finishGame: function () {
-    	let t = timer.getTime();
-    	game.time = `${t.h.innerHTML}:${t.m.innerHTML}:${t.s.innerHTML}`;
-    	
-    	game.score = Math.floor(temporaryData.areaSize * 10 +
-    		(temporaryData.areaSize * 100 - 
-    		(temporaryData.wrongAttempt - temporaryData.areaSize/2)) *
-    		temporaryData.areaSize * 100/
-    		(+t.h.innerHTML + +t.m.innerHTML * 60 + +t.s.innerHTML * 3600));
-
-		let	newStorage = ({[temporaryData.areaSize + '']: [{
+	finishGame: function () {
+		let t = timer.getTime(),
+			newStorage = ({[temporaryData.areaSize + '']: [{
 				user: game.userName, 
 				date: new Date(), 
 				score: game.score}
 				]}
-			);
-		let storage = menu.getStorage();
-		if(storage === null) {
-			storage = newStorage;
-			
-		}else if(storage !== null && storage[temporaryData.areaSize] === undefined){
-			storage[temporaryData.areaSize] = newStorage[temporaryData.areaSize]
-		}else{
-			storage[temporaryData.areaSize][storage[temporaryData.areaSize].length] 
-			= newStorage[temporaryData.areaSize][0]
-		}
-		menu.setStorage(storage); 
+			),
+		 	storage = menu.getStorage(),
+		 	data = document.createElement('div');
 
-	    console.log(
-	    	`${game.userName}, you win, 
-	    	your time: ${game.time},
-	    	your score: ${game.score}`
-	    	);
-	    timer.stop();
-	    temporaryData.rightAttempt = temporaryData.wrongAttempt = 0;
+		game.time = `${t.h.innerHTML}:${t.m.innerHTML}:${t.s.innerHTML}`;
+		game.score = Math.floor(temporaryData.areaSize * 10 +
+			(temporaryData.areaSize * 100 - 
+			(temporaryData.wrongAttempt - temporaryData.areaSize/2)) *
+			temporaryData.areaSize * 100/
+			(+t.h.innerHTML + +t.m.innerHTML * 60 + +t.s.innerHTML * 3600));
+
+		if(storage === null) {
+			storage = newStorage;	
+		}else if(storage !== null 
+			&& storage[temporaryData.areaSize] 
+				=== undefined){
+				storage[temporaryData.areaSize] 
+				= newStorage[temporaryData.areaSize]
+		}else{
+			storage[temporaryData.areaSize][storage[temporaryData
+			.areaSize].length] = newStorage[temporaryData.areaSize][0]
+		}
+
+		menu.setStorage(storage); 
+		timer.stop();
+		data.innerHTML = `<h2>${game.userName}, you win</h2> 
+			<p>Your time: ${game.time}</p>
+			<p>Your score: ${game.score}</p>`
+		popUp.content(data, function () {controller.init()});
+		temporaryData.rightAttempt = temporaryData.wrongAttempt = 0;
 	},
 
 	checkPair: function(className) {
-  
 		let x = document.getElementsByClassName("flipped");
 		setTimeout(function() {
 
 			for(let i = 1; i >= 0; i--) {
-			  	x[0].className = className;
+				x[0].className = className;
 			}
 		},600);
 	},
 
 	userScores: function () {
 		let storage = menu.getStorage();
+		let tab = {};
 
 		for (key in storage) {
-			let tab = `${Math.sqrt(+key)}x${Math.sqrt(+key)}`;
-			// console.log(storage[key])	
-			let arr = storage[key].sort((a, b) => b.score - a.score)
-			let row;
-			// console.log(arr)	
+			let tabName = `${Math.sqrt(+key)}x${Math.sqrt(+key)}`;
+			let arr = (storage[key].sort((a, b) => b.score - a.score)).slice(0, 9)
+			let rows = '';
+
 			for (let i = 0; i < arr.length; i++) {
-				// data = storage[key][i];
-				row = new Results(arr[i]);
-				console.log(row.cell())	
+				rows = rows.concat((new Results(arr[i])).cell());
 			}
-			// let row = new Results(storage[key][i]);
-			
-			
+
+			tab[tabName] = (new ResultTabCreate(rows))
+				.tabCreate(tabName, rows);
+			pageResults.pages = tab;
+			console.log(tab)
 		}
+
+		cardView.pagesRender();
+	},
+	chooseTab: function (event) {
+		let resultsContainer = document.getElementById('results'),
+			tabsContainer = document.getElementById('tabs');
+
+		for (var i = 0; i < tabsContainer.children.length; i++) {
+			tabsContainer.children[i].className = '';
+		}
+
+		event.target.className = 'active';
+		resultsContainer.innerHTML = pageResults.pages[tabsContainer
+			.getElementsByClassName('active')[0].innerHTML];
 	}
 }
 
@@ -218,11 +289,11 @@ let cardView = {
 	init: function() {
 		let cardsInstances = [];
 		temporaryData.areaSize = menu.area
-			.options[menu.area
-			.options.selectedIndex].value;
+			.options[menu.area.options.selectedIndex].value;
 		controller.changeArea();
+		controller.changeUser();
 		
-	    for (let i = 1; i <= temporaryData.areaSize/2; i++) {
+		for (let i = 1; i <= temporaryData.areaSize/2; i++) {
 			let instance = new CardModel({
 				value: i <= 50 ? i : i - 50, 
 				img: `./img/heroes/${i <= 50 ? i : i - 50}.jpg`
@@ -231,10 +302,11 @@ let cardView = {
 			cardsInstances.push(instance);
 		};
 
-		temporaryData.cards = cardsInstances.concat(cardsInstances);
+		temporaryData.cards = cardsInstances
+			.concat(cardsInstances);
 
-	    cardView.render();
-	    
+		cardView.render();
+		
 	},
 
 	render: function () {
@@ -242,25 +314,24 @@ let cardView = {
 		let cardsArea = menu.container;
 
 		while (container.firstChild) {
-    		cardsArea.removeChild(container.firstChild);
+			cardsArea.removeChild(container.firstChild);
 		}
 
 		temporaryData.cards.sort(this.shuffle);
-		// timer.stop();
+		timer.start();
 		
 		for (let i = 0; i < temporaryData.cards.length; i++) {
 			let card = document.createElement('div');
 			card.dataset.value = temporaryData.cards[i]._value;
 			card.dataset.view = temporaryData.cards[i]._view;
-			card.style.background = `url('./${temporaryData.cards[i]._img}') 
-				center center no-repeat #fff`;
+			card.style.background = `
+				url('./${temporaryData.cards[i]._img}') 
+				center center no-repeat #fff
+				`;
 			cardsArea.appendChild(card);
 
 			card.onclick = this.flip;
 		}
-
-		
-			
 	},
 
 	flip: function (e) {
@@ -276,50 +347,55 @@ let cardView = {
 					temporaryData.resultsArray.push(result);
 
 				}
-
-				// clearInterval(Interval);
-				// Interval = setInterval(startTimer, 10);
 			}
 
 			if (temporaryData.resultsArray.length === 2) {
 
-				if (temporaryData.resultsArray[0] === temporaryData.resultsArray[1]) {
+				if (temporaryData.resultsArray[0] 
+					=== temporaryData.resultsArray[1]) {
 					controller.checkPair("correct");
 					temporaryData.resultsArray = [];
 					temporaryData.rightAttempt++;
 
-					if(temporaryData.rightAttempt === temporaryData.areaSize/2) {
-						controller.finishGame();
+					if(temporaryData.rightAttempt 
+						=== temporaryData.areaSize/2) {
+							controller.finishGame();
 					}
+
 				} else {
 					controller.checkPair("reverse");
 					temporaryData.resultsArray = [];
 					temporaryData.wrongAttempt++;
 				}
 			}
+
 			if(x.length === 2) {
 				return;
 			}
 		},
 
- 	shuffle: function(a, b) {
+	shuffle: function(a, b) {
 		return Math.random() - 0.5;
+	},
+
+	pagesRender: function () {
+		let data = document.createElement('div'),
+			tabs = document.createElement('div'),
+			results = document.createElement('div');
+			tabs.id = 'tabs';
+			results.id = 'results';
+			
+			timer.pause();
+		for (k in pageResults.pages) {
+			tabs.innerHTML = (tabs.innerHTML).concat(`<div>${k}</div>`)	
+		}
+		data.appendChild(tabs);
+		data.appendChild(results);
+		tabs.firstChild.className = 'active';
+		popUp.content(data, function () {controller.continueGame()})
+		data.onclick = controller.chooseTab;
+		results.innerHTML = pageResults.pages[tabs.firstChild.innerHTML];
 	}
-
-	// setUserName: function () {
-		
-	// }
-
-	
-
-	// console.log(data.cards);
-	// render()
-	// 		subscribe to model changes
-	// cardInstance.addSubscriber( render );
-	// photoEl.addEventListener( "click", function () {
-	// 	//controller do some work
-	// 	cardController.handleEvent( "click", photoModel );
-	// });
 
 };
 // cardView()
@@ -329,19 +405,3 @@ let cardView = {
 
 
 controller.init();
-
-	//    var CardController = function () {
-	//     this.handleEvent = function(){
-	//         //do something important
-	//         //e.g. update model
-	//     }
-	//     this.init = function () {
-	//         var cardInstance = new PhotoModel({
-	//             name: 'myPhoto',
-	//             date: 'today',
-	//             url: 'http://example.com/mPhoto.jpg'
-	//         });
-	//         var photoViewInstance = new cardView(cardInstance, this);
-	//     }
-	//     return this;
-	// };
